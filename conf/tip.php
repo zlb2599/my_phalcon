b9445cc57ab50db0803e945f332199ce5dc9f5b3
zhanglibo <zhanglibo@xiaohe.com>
2018-05-22 16:42:07 +0800

    delete

--- conf/tip.php
+++ conf/tip.php
@@ -13,26 +13,4 @@ return [
     'API_COMM_004' => ['PhalconException', 'PhalconException'],
     'API_COMM_404' => ['not found', 'not found'],
 
-    'API_CLASS_001' => ['商家ID为必填项', '商家不正确'],
-    'API_CLASS_002' => ['教师ID不能为空', '教师ID不能为空'],
-    'API_CLASS_003' => ['商品ID不能为空', '商品ID不能为空'],
-
-    //员工
-    'API_EMPLOYEE_001' => ['员工ID不能为空', '员工ID不能为空'],
-
-    'API_COMMENT_001' => ['课次ID不能为空', '课次ID不能为空'],
-    'API_COMMENT_002' => ['学员ID不能为空', '学员ID不能为空'],
-    'API_COMMENT_003' => ['角色类型ID不能为空', '角色类型ID不能为空'],
-    'API_COMMENT_004' => ['身份类型ID不能为空', '身份类型ID不能为空'],
-    'API_COMMENT_005' => ['项目ID不能为空', '项目ID不能为空'],
-
-    'API_MEMBER_001'  => ['会员ID不能为空', '会员ID不能为空'],
-
-    //作业
-    'API_HOMEWORK_001' => ['商家ID为必填项', '商家不正确'],
-    'API_HOMEWORK_002' => ['平台ID为必填项', '平台不正确'],
-    'API_HOMEWORK_003' => ['员工ID为必填项', '员工不正确'],
-
-
-
 ];
